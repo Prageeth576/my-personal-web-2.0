75dfdfc21b6975ad35562dd7e46911b37e846804
Prageeth576 <ygprageethchamara2002@gmail.com>
2024-01-18 13:56:25 +0530

    first-commit

---
+++ js/sript.js
@@ -0,0 +1,9 @@
+/*---------- responsive nav bar ----------*/
+function myFunction(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav_bar"){
+        x.className += " responsive";
+    } else {
+        x.className = "nav_bar";
+    }
+}
